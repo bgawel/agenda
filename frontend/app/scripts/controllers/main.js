3d5c732acfae4e9c5fb2298406839c6e9fff42cc
bgawel <bartek.gawel@gmail.com>
2014-01-13 10:51:21 +0100

    Initial version of the front screen

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('frontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'SitePoint'
+    ];
+    $scope.toggleRightPanel = function () {
+    	$scope.rightPanel = ($scope.rightPanel === "activeRight") ? "" : "activeRight";
+    };
+    $scope.toggleLeftPanel = function () {
+    	$scope.leftPanel = ($scope.leftPanel === "activeLeft") ? "" : "activeLeft";
+    };    
+  });
